b6bf9327420133dec6c0046256a4ec7f6ccc0610
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-01-03 21:12:33 -0800

    adjusting auth methond to add cookieparser

--- __tests__/entries.test.js
+++ __tests__/entries.test.js
@@ -8,31 +8,11 @@ const Notes = require('../lib/models/Notes');
 const User = require('../lib/models/User'); 
 
 describe('notes routes', () => {
-
+  let access;
   beforeAll(async() => {
     connect(); 
-  });
-
-  beforeEach(() => {
-    return mongoose.connection.dropDatabase(); 
-  }); 
-
-  
-  let note; 
-  beforeEach(async() => {
-    note = await Notes.create({
-      category: 'science', 
-      entry: 'science is hella cool'
-    });
-  });
+    access = request.agent(app);
     
-  afterAll(() => {
-    return mongoose.connection.close(); 
-  }); 
-
-  it('can create a new note when logged in', async() => {
-    let access = request.agent(app); 
-
     await User
       .create({
         email: 'calvin@coolidge.com', 
@@ -45,13 +25,62 @@ describe('notes routes', () => {
         email: 'calvin@coolidge.com', 
         password: 'president'
       });
+  });
+  
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase(); 
+  }); 
 
+  afterAll(() => {
+    return mongoose.connection.close(); 
+  });
+  
+  let note; 
+  beforeEach(async() => {
+    note = await Notes.create({
+      category: 'science', 
+      entry: 'science is hella cool'
+    });
+  });
+
+  console.log(note);
+  
+
+  it('can create a new note when logged in', async() => {
     return access
       .post('/api/v1/notes')
       .send({
         category: 'Math',
         entry: 'is number 1'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          category: 'Math',
+          entry: 'is number 1',
+          __v: 0
+        });
       });
   });
 
+  it('requires auth', () => {
+    return request(app)
+      .get('/api/v1/note')
+      .then(res => {
+        expect(res.statusCode).toEqual(500); 
+      });
+  });
+
+  it('can get all user notes', () => {
+    return request(app)
+      .get(`/api/v1/notes/${note.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          category: 'Math',
+          entry: 'me plus you is two',
+          __v: 0
+        });
+      });
+  });
 });
